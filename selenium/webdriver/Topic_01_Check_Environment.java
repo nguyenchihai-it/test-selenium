41f0b0efc53c89a4266a557845fcb591cb7c4217
hainguyen <hainguyen@DESKTOP-P1Q8IJF>
2022-05-05 20:11:32 +0700

    test slack

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -42,7 +42,7 @@ public class Topic_01_Check_Environment {
 	public void TC_03_LoginFormDisplayed() {
 		// Login form displayed
 		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
-		//
+		
 	}
 
 	@AfterClass
